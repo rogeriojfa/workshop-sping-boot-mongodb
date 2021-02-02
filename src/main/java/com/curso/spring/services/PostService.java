7d141af1361736585351f11c8ea7a7fa91a8a534
Rogério Ferreira <rogeriojfa@gmail.com>
2021-02-02 19:34:58 -0300

    consulta com varios critérios

--- src/main/java/com/curso/spring/services/PostService.java
+++ src/main/java/com/curso/spring/services/PostService.java
@@ -3,8 +3,11 @@ package com.curso.spring.services;
 import com.curso.spring.domain.Post;
 import com.curso.spring.repositoty.PostRepository;
 import com.curso.spring.services.exception.ObjectNotfoundException;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +24,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return pstRep.searchTitle(text);
+    }
+
+    public List<Post> fullTextSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+        return pstRep.fullSearch(text, minDate, maxDate);
     }
 }
