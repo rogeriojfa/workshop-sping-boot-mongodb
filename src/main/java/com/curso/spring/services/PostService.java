b0e6a7ebfabc90403672b063661110f2c6a319c8
Rogério Ferreira <rogeriojfa@gmail.com>
2021-02-02 18:09:44 -0300

    OFT MINGW64 /c/Projetos/curso (master) $ git commit -m

--- src/main/java/com/curso/spring/services/PostService.java
+++ src/main/java/com/curso/spring/services/PostService.java
@@ -1,17 +1,12 @@
 package com.curso.spring.services;
 
 import com.curso.spring.domain.Post;
-import com.curso.spring.domain.User;
 import com.curso.spring.repositoty.PostRepository;
 import com.curso.spring.services.exception.ObjectNotfoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
-
-
-
 
 @Service
 public class PostService {
@@ -25,6 +20,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text){
-        return pstRep.findByTitleContainingIgnoreCase(text);
+        return pstRep.searchTitle(text);
     }
 }
